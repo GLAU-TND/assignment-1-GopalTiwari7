0d2ecb2bbf1d9b53fb4b3c50958493ff0a206f27
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:47:12 +0530

    create two arrayLists

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,11 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
 }
