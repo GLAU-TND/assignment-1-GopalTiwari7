aa98dcf64ca846e6343cb2613ea2d042792e46a8
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:11:59 +0530

    create getters and setters

--- src/person/Person.java
+++ src/person/Person.java
@@ -19,6 +19,42 @@ public class Person implements Comparable<Person> {
         this.phoneNumber = phoneNumber;
         this.list = list;
     }
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public List<Long> getList() {
+        return list;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        list.add(phoneNumber);
+    }
 
     @Override
     public int compareTo(Person o) {
