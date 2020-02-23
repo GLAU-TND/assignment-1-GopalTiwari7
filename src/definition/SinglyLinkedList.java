797fad1a59b62482e9883a490cdc00b1ae5bd336
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:33:40 +0530

    Create a Node class

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -4,6 +4,7 @@ import adt.ADT;
 
 public class SinglyLinkedList<E> implements ADT<E> {
 
+
     @Override
     public void add(E item) {
 
@@ -27,5 +28,29 @@ public class SinglyLinkedList<E> implements ADT<E> {
     @Override
     public void print() {
 
+    }
+
+    //private Node class to provide the node.
+    private static class Node<E> implements Comparable<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        @Override
+        public int compareTo(E e) {
+            return 0;
+        }
     }
 }
