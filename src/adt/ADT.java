1346927ca0b50ed2d6e602bdfc069ae70be9b6a5
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:00:33 +0530

    Create a method to remove elements in the list

--- src/adt/ADT.java
+++ src/adt/ADT.java
@@ -5,5 +5,7 @@ public interface ADT<E> {
 
     void add(E item);     //  This method will add the item in the list.
 
+    void remove();       //  This method will remove the item in the list.
+
 
 }
