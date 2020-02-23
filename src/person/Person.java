21bdfc96e96c2695ffb5a8b69d84eb46d79ce94b
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:10:59 +0530

    Create constructor of person class

--- src/person/Person.java
+++ src/person/Person.java
@@ -10,6 +10,16 @@ public class Person implements Comparable<Person> {
     private long phoneNumber;
     List<Long> list = new ArrayList<>();
 
+    public Person() {
+    }
+    public Person(String fName, String lName, String email, long phoneNumber, List<Long> list) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.list = list;
+    }
+
     @Override
     public int compareTo(Person o) {
         return 0;
