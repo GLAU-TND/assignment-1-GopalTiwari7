6417714789e2945b6f433d60e7b7ce2ad4444405
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:08:45 +0530

    Initialise the fields

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,5 +1,13 @@
 package person;
 
-public class Person {
+public class Person implements Comparable<Person> {
+    private String fName;
+    private String lName;
+    private String email;
+    private long phoneNumber;
 
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
 }
