4440ee577fdbfa0808604b3848a0fbed3cd3bb72
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:26:56 +0530

    implements ADT inteface

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -1,4 +1,31 @@
 package definition;
 
-public class SinglyLinkedList {
+import adt.ADT;
+
+public class SinglyLinkedList<E> implements ADT<E> {
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
