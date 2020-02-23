cb1083d3f551c5dbb72b1abf4250a3cebd5722fc
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:34:29 +0530

    Create a getNode method

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -3,6 +3,24 @@ package definition;
 import adt.ADT;
 
 public class SinglyLinkedList<E> implements ADT<E> {
+    private int size = 0;
+    private Node<E> head = null;
+
+    public int getSize() {
+        return size;
+    }
+    //This method give the node according to index.
+    public Node<E> getNode(int index) {
+        Node<E> response = head;
+        if (index < 0 && index > size) {
+            throw new IndexOutOfBoundsException(Integer.toString(index));
+        } else {
+            for (int i = 0; i < index && head != null; i++) {
+                response = response.next;
+            }
+        }
+        return response;
+    }
 
 
     @Override
@@ -54,3 +72,4 @@ public class SinglyLinkedList<E> implements ADT<E> {
         }
     }
 }
+
