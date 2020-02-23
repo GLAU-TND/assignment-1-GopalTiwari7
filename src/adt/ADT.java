d6afb8197ae20decf1130ae064eee0a09bf07c0d
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-02-23 19:02:13 +0530

    Create a method to search elements in the list

--- src/adt/ADT.java
+++ src/adt/ADT.java
@@ -7,5 +7,12 @@ public interface ADT<E> {
 
     void remove();       //  This method will remove the item in the list.
 
+    int search(E item);
+    /*
+       This method will search an item in the list.
+
+       @param item the item to be searched inside the list.
+     */
+
 
 }
